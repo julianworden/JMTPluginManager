80b64b01413ee0d89ab847188041e7687e14ba82
Julian Worden <93101937+julianworden@users.noreply.github.com>
2024-03-11 19:01:42 -0700

    Initial Commit

---
+++ PluginManager/PluginManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PluginManagerApp.swift
+//  PluginManager
+//
+//  Created by Julian Worden on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PluginManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
